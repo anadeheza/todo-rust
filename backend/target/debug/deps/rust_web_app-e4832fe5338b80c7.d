35c9a3b483b5755f820564e6f939f303bd362a68
Ana <adehezaflaherty@gmail.com>
2026-06-05 10:08:41 -0300

    for deployment

--- backend/target/debug/deps/rust_web_app-e4832fe5338b80c7.d
+++ backend/target/debug/deps/rust_web_app-e4832fe5338b80c7.d
@@ -1,5 +1,5 @@
-C:\Users\adehe\proyectos\otros\rust_web_app\target\debug\deps\rust_web_app-e4832fe5338b80c7.d: src\main.rs
+C:\Users\adehe\proyectos\otros\rust_web_app\backend\target\debug\deps\rust_web_app-e4832fe5338b80c7.d: src\main.rs
 
-C:\Users\adehe\proyectos\otros\rust_web_app\target\debug\deps\librust_web_app-e4832fe5338b80c7.rmeta: src\main.rs
+C:\Users\adehe\proyectos\otros\rust_web_app\backend\target\debug\deps\librust_web_app-e4832fe5338b80c7.rmeta: src\main.rs
 
 src\main.rs:
